bd3d2b25ba3da3528b02b6fa502d5bae69b05cea
VieetBubbles <537@holbertonschool.com>
2019-05-14 20:03:27 +0000

    Created a function that searches for the correct string inputted by the user

---
+++ get_monty_function.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * get_opcode - that selects the correct function to perform the
+ * operation asked by the user
+ * @s: the string passed as argument to the program
+ *
+ * Return: the function pointer to an operator
+ */
+
+int (*get_opcode(char *s))
+{
+	op_t ops[] = {
+		{"push", op_push},
+		{"pall", op_pall},
+		{"pint", op_pint},
+		{"pop", op_pop},
+		{"swap", op_swap},
+		{"add", op_add},
+		{"nop", op_nop},
+		{NULL, NULL}};
+	int i = 0;
+
+	while (i != 7)
+	{
+		if (!(strcmp(ops[i].op, s)))
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
