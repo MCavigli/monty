165e7dd30f848e1969e94df0170519dbb27d2958
VieetBubbles <537@holbertonschool.com>
2019-05-15 21:45:39 +0000

    Fix merge conflict error

---
+++ monty.h
@@ -0,0 +1,56 @@
+#ifndef __MONTY__H__
+#define __MONTY__H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* global variable */
+typdef struct glo_s
+{
+	FILE *fp;
+	char *line_buff;
+	char **bigb;
+	int node_data;
+} glo_t;
+extern glo_t glo;
+
+char **parse_line();
+
+int get_opcode(stack_t **stack, unsigned int line_number);
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+
+#endif
