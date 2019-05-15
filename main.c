53e3acbb8ac1f7da6766d10e2e1a184aa3648d44
Marc Cavigli <mcavigli@gmail.com>
2019-05-15 22:56:30 +0000

    Added frees

--- main.c
+++ main.c
@@ -34,12 +34,13 @@ int main(int argc, char **argv)
 			glo.node_data = atoi(glo.bigb[1]);
 		check = get_opcode(&head, counter);
 
-		op_check(check, counter, bigb, line_buff);
+		op_check(check, counter);
 
 		lines = getline(&glo.line_buff, &line_buff_size, glo.fp);
 	}
 	free(glo.bigb);
 	free(glo.line_buff);
 	fclose(glo.fp);
+	free(head);
 	return (0);
 }
