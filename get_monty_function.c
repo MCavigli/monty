254f754f77592e2387de803eaa640fa98ff526a6
VieetBubbles <537@holbertonschool.com>
2019-05-15 21:33:27 +0000

    Changed to match development branch file

--- get_monty_function.c
+++ get_monty_function.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+glo_t glo;
 
 /**
  * get_opcode - that selects the correct function to perform the
@@ -8,17 +9,21 @@
  * Return: the function pointer to an operator
  */
 
-int get_opcode(stack_t **stack, unsigned int line_number, char *str)
+int get_opcode(stack_t **stack, unsigned int line_number)
 {
 	instruction_t ops[] = {
 		{"push", op_push},
 		{"pall", op_pall},
+		{"pint", op_pint},
+		{"pop", op_pop},
+		{"add", op_add},
+		{"swap", op_swap},
 		{NULL, NULL}};
 	int i = 0;
 
-	while (i != 2)
+	while (i != 5)
 	{
-		if (!(strcmp(ops[i].opcode, str)))
+		if (!(strcmp(ops[i].opcode, glo.bigb)))
 		{
 			ops[i].f(stack, line_number);
 			break;
