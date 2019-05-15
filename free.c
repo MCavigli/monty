3fd6dc2fc8285c63572ed79eeb422d3ab5ab588f
Marc Cavigli <mcavigli@gmail.com>
2019-05-15 22:06:52 +0000

    Added function to free list

--- free.c
+++ free.c
@@ -10,3 +10,21 @@ void free_buff()
 	free(glo.line_buff);
 	fclose(glo.fp);
 }
+
+/**
+ * free_stack - frees a list
+ * @head: pointer to head of the list
+ * Return: nothing
+ */
+
+void free_stack(stack_t *head)
+{
+	stack *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
