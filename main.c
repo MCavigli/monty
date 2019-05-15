72ec3918ad4d7b13bed7cb51b612910ed156aea6
Marc Cavigli <mcavigli@gmail.com>
2019-05-15 22:30:03 +0000

    Cleaned up code

--- main.c
+++ main.c
@@ -17,43 +17,14 @@ int main(int argc, char **argv)
 	glo.line_buff = NULL;
 	glo.bigb = NULL;
 
-/*
-	stack_t *head = NULL;
-	char *line_buff = NULL;
-	char **bigb = NULL;
-	size_t line_buff_size = 0;
-	FILE *fp;
-	unsigned int counter = 0;
-	ssize_t lines;
-	int check;
-*/
 	argc_check(argc);
-/*
-	if (argc != 2)
-	{
-		dprintf("USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-*/
 
 	glo.fp = fopen(argv[1], "r");
 	open_check(argv);
-/*
-	if (fp == NULL)
-	{
-		printf("Error: Can't open file \n");
-		exit(EXIT_FAILURE);
-	}
-*/
+
 	lines = getline(&glo.line_buff, &line_buff_size, glo.fp);
 	line_check(lines);
-/*
-	if (lines == -1)
-	{
-		free(line_buff);
-		exit(0);
-	}
-*/
+
 	while (lines >= 0)
 	{
 		glo.bigb = NULL;
@@ -63,20 +34,7 @@ int main(int argc, char **argv)
 			glo.node_data = atoi(glo.bigb[1]);
 		check = get_opcode(&head, counter);
 
-		if (check == 0)
-		{
-/*
-  op_check(check, counter, bigb, line_buff);
-*/
-
-			dprintf(2, "L%u: unknown instruction %s\n", counter, glo.bigb[0]);
-			free(glo.bigb);
-			free(glo.line_buff);
-			fclose(glo.fp);
-			exit(EXIT_FAILURE);
-		}
-
-
+		op_check(check, counter, bigb, line_buff);
 
 		lines = getline(&glo.line_buff, &line_buff_size, glo.fp);
 	}
