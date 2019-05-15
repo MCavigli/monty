9aef922cff19059f185a3ed270ad5702e83094df
VieetBubbles <537@holbertonschool.com>
2019-05-15 21:44:07 +0000

    Testing to resolve git errors

--- op_functions.c
+++ op_functions.c
@@ -1,5 +1,5 @@
 #include "monty.h"
-int node_data;
+glo_t glo;;
 
 /**
  * op_push - function that push an element to the stack
@@ -12,17 +12,19 @@ int node_data;
 void op_push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node;
-	(void)line_number;
 
 	if (!stack)
-		exit(1);
+	{
+		dprintf(2, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 	new_node = malloc(sizeof(stack_t));
 	if (!new_node)
 	{
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
-	new_node->n = node_data;
+	new_node->n = glo.node_data;
 	new_node->next = NULL;
 	new_node->prev = NULL;
 
@@ -50,6 +52,7 @@ void op_push(stack_t **stack, unsigned int line_number)
 
 void op_pall(stack_t **stack, unsigned int line_number)
 {
+	stack_t *tmp = *stack;
 	(void)line_number;
 
 	while (*stack)
@@ -57,4 +60,60 @@ void op_pall(stack_t **stack, unsigned int line_number)
 		printf("%d\n", (*stack)->n);
 		*stack = (*stack)->next;
 	}
+	*stack = tmp;
+}
+
+/**
+ * op_pint - function that prints the value at the top of the stack
+ * @stack: a pointer to the head of a linked list
+ * @line_number: the line number
+ *
+ * Return: nothing
+ */
+
+void op_pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack)
+	{
+		printf("%d\n", (*stack)->n);
+	}
+	else
+	{
+		dprintf(2, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * op_pop - function that removes the top element of the stack
+ * @stack: a pointer to the head of a linked list
+ * @line_number: the line number
+ *
+ * Return: nothing
+ */
+
+void op_pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+	(void)line_number;
+
+	while(*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+		break;
+	}
+}
+
+/**
+ *
+ *
+ */
+
+void op_add(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	return;
 }
