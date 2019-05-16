acdd00cd1f57287f5632d8015e7c406c1748d172
Marc Cavigli <mcavigli@gmail.com>
2019-05-16 03:28:01 +0000

    Added code to check for non-integers

--- parse_line.c
+++ parse_line.c
@@ -8,12 +8,8 @@ glo_t glo;
 char *parse_line(int c)
 {
 	char *token = NULL;
-/*
 	char *next_token = NULL;
-*/
 	int i = 0;
-	(void)c;
-
 
 	while (glo.line_buff[i])
 		i++;
@@ -21,11 +17,41 @@ char *parse_line(int c)
 	if (glo.line_buff[i - 1] == '\n')
 		glo.line_buff[i - 1] = '\0';
 
+	i = 0;
 	token = strtok(glo.line_buff, " \t");
 	if (strcmp(token, "push") == 0)
-		glo.node_data = atoi(strtok(NULL, " \t"));
+	{
+		next_token = strtok(NULL, " \t");
+		if (next_token == NULL)
+		{
+			dprintf(2, "L%i: usage: push integer\n", c);
+			exit(EXIT_FAILURE);
+		}
+		while (next_token[i])
+		{
+			if (i == 0 && next_token[i] == '-')
+			{
+				i++;
+				continue;
+			}
+			if (next_token[i] >= 48 && next_token[i] <= 57)
+			{
+				i++;
+				continue;
+			}
+			else
+			{
+				dprintf(2, "L%i: usage: push integer\n", c);
+				exit(EXIT_FAILURE);
+			}
+		}
+		glo.node_data = atoi(next_token);
+	}
 	return (token);
-
+/*
+	glo.node_data = atoi(strtok(NULL, " \t"));
+	return (token);
+*/
 /*
 		next_token = strtok(NULL, " \t");
 		while (next_token[i])
