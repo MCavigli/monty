5a7bcf023f77b478f6a872ca65a9e41d42c69906
Marc Cavigli <mcavigli@gmail.com>
2019-05-16 20:31:03 +0000

    Added errors for mul

--- errors2.c
+++ errors2.c
@@ -21,3 +21,14 @@ void div_error(unsigned int c)
 	dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n", c);
 	exit(EXIT_FAILURE);
 }
+
+/**
+ * mul_error - Error functionality for op_mul()
+ * @c: line count
+ * Return: nothing
+ */
+void mul_error(unsigned int c)
+{
+	dprintf(STDERR_FILENO, "L%u: can't mul, stack too short\n", c);
+	exit(EXIT_FAILURE);
+}
