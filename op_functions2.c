957fa60323c25c1159e66cc78174a249c61a3275
VieetBubbles <537@holbertonschool.com>
2019-05-16 08:01:49 +0000

    Added a pointer called current to the swap function for the linked list

--- op_functions2.c
+++ op_functions2.c
@@ -3,39 +3,28 @@
 /**
  * op_swap - function that swaps the first 2 nodes in a linked list
  * @stack: a pointer to the head of a linked list
- * @line_number: the line number
+ * @l: the line number
  *
  * Return: nothing
  */
 
-void op_swap(stack_t **stack, unsigned int line_number)
+void op_swap(stack_t **stack, unsigned int l)
 {
-	stack_t *node1 = *stack;
-	stack_t *node2 = (*stack)->next;
-	stack_t *temp = NULL;
-	stack_t *node3 = NULL;
+	stack_t *current = *stack;
+	stack_t *second_node;
+	int temp;
 
-	if (*stack && (*stack)->next)
+	if (current || current->next)
 	{
-		node3 = node2->next;
-		temp = node1->prev;
-
-		node1->next = node3;
-		node1->prev = node2;
-
-		node2->prev = temp;
-		node2->next = node1;
-
-		if (node3 != NULL)
-		{
-			node3->prev = node1;
-		}
-		*stack = node2;
+		second_node = current->next;
+		temp = current->n;
+		current->n = second_node->n;
+		second_node->n = temp;
 	}
 
 	else
 	{
-		dprintf(2, "L%u: can't swap, stack too short\n", line_number);
+		dprintf(STDERR_FILENO, "L%u: can't swap, stack too short\n", l);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
