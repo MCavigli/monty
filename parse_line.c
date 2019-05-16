99db36f4c6e5672d6f72a73c595f3130f37c5b98
Marc Cavigli <mcavigli@gmail.com>
2019-05-16 03:29:56 +0000

    Fixed betty errors

--- parse_line.c
+++ parse_line.c
@@ -2,9 +2,9 @@
 glo_t glo;
 /**
  * parse_line - tokenizes the line
- * Return: an array of strings that holds the line and an argument if necessary
+ * @c: line number
+ * Return: a token that holds the correct command
  */
-
 char *parse_line(int c)
 {
 	char *token = NULL;
@@ -13,10 +13,8 @@ char *parse_line(int c)
 
 	while (glo.line_buff[i])
 		i++;
-
 	if (glo.line_buff[i - 1] == '\n')
 		glo.line_buff[i - 1] = '\0';
-
 	i = 0;
 	token = strtok(glo.line_buff, " \t");
 	if (strcmp(token, "push") == 0)
@@ -48,62 +46,4 @@ char *parse_line(int c)
 		glo.node_data = atoi(next_token);
 	}
 	return (token);
-/*
-	glo.node_data = atoi(strtok(NULL, " \t"));
-	return (token);
-*/
-/*
-		next_token = strtok(NULL, " \t");
-		while (next_token[i])
-		{
-			if (next_token[i] == '-')
-			{
-				i++;
-				continue;
-			}
-			if (isdigit(next_token[i]))
-			{
-				i++;
-				continue;
-			}
-			else
-			{
-				dprintf(2, "L%i: usage: push integer\n", c);
-				exit(EXIT_FAILURE);
-			}
-
-
-		}
-*/
-
-/*
-	char **big_buff = malloc(10 * (sizeof(char *)));
-	char *token;
-	int i = 0;
-
-	if (!big_buff)
-	{
-		dprintf(2, "Error: malloc failed\n");
-		free(glo.line_buff);
-		fclose(glo.fp);
-		exit(EXIT_FAILURE);
-	}
-
-	while (glo.line_buff[i])
-		i++;
-
-	if (glo.line_buff[i - 1] == '\n')
-		glo.line_buff[i - 1] = '\0';
-
-	token = strtok(glo.line_buff, " \t");
-	i = 0;
-	while (token)
-	{
-		big_buff[i] = token;
-		i++;
-		token = strtok(NULL, " \t");
-	}
-	big_buff[i] = NULL;
-	return (big_buff);
-*/
 }
