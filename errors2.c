a25a24334ef556f6416ab871f0fcb058762f3a11
Marc Cavigli <mcavigli@gmail.com>
2019-05-16 20:14:47 +0000

    Added functionality for div

--- errors2.c
+++ errors2.c
@@ -10,3 +10,14 @@ void sub_error(unsigned int c)
 	dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n", c);
 	exit(EXIT_FAILURE);
 }
+
+/**
+ * div_error - Error functionality for op_div()
+ * @c: line count
+ * Return: nothing
+ */
+void div_error(unsigned int c)
+{
+	dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n", c);
+	exit(EXIT_FAILURE);
+}
