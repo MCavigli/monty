2f47a6d4f25ada963eee0a9fcfce5ac537719548
Marc Cavigli <mcavigli@gmail.com>
2019-05-16 00:58:49 +0000

    Changed code to strtok better

--- parse_line.c
+++ parse_line.c
@@ -2,12 +2,32 @@
 glo_t glo;
 /**
  * parse_line - tokenizes the line
- * @line: the line
  * Return: an array of strings that holds the line and an argument if necessary
  */
 
-char **parse_line()
+char *parse_line(int c)
 {
+	char *token = NULL;
+	int i = 0;
+
+	while (glo.line_buff[i])
+		i++;
+	if (glo.line_buff[i - 1] == '\n')
+		glo.line_buff[i - 1] = '\0';
+
+	token = strtok(glo.line_buff, " \t");
+	if (strcmp(token, "push") == 0)
+	{
+		glo.node_data = atoi(strtok(NULL, " \t"));
+		if (!(glo.node_data >= -1000 && glo.node_data <= 1000))
+		{
+			dprintf(2, "L%i: usage: push integer\n", c);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return (token);
+/*
 	char **big_buff = malloc(10 * (sizeof(char *)));
 	char *token;
 	int i = 0;
@@ -36,4 +56,5 @@ char **parse_line()
 	}
 	big_buff[i] = NULL;
 	return (big_buff);
+*/
 }
